e80f0218a0b3a8a8bb4c9214b3aa80a2235f1db9
Fito Toledano <fito.toledano@gmail.com>
2021-02-12 04:42:27 +0100

    initial commit

--- Nodeum/ContentView.swift
+++ Nodeum/ContentView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                
+            }
+        }
     }
 }
 
