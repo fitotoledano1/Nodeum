d84d7c9ed0ddb3f3d221834f3a6939758cf86fff
Fito Toledano <fito.toledano@gmail.com>
2021-02-12 04:23:45 +0100

    Initial Commit

---
+++ Nodeum/NodeumApp.swift
@@ -0,0 +1,17 @@
+//
+//  NodeumApp.swift
+//  Nodeum
+//
+//  Created by Fito Toledano on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct NodeumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
