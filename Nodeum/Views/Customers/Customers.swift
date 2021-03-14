375282e3f5f6f4c58b5fd9d153ebafcbe65db8ab
Fito Toledano <fito.toledano@gmail.com>
2021-03-14 17:10:25 +0100

    wip

--- Nodeum/Views/Customers/Customers.swift
+++ Nodeum/Views/Customers/Customers.swift
@@ -8,27 +8,34 @@
 import SwiftUI
 
 struct Customers: View {
-        
+    
     private let customers = NodeumData.customers
-        
-    let rows = [
-        GridItem(.fixed(50)),
-        GridItem(.fixed(50))
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 80, maximum: 84),
+                 spacing: 12,
+                 alignment: .leading)
     ]
     
     var body: some View {
         NavigationView {
-            LazyHGrid(rows: rows, alignment: .center) {
+            LazyVGrid(columns: columns, alignment: .center) {
                 ForEach(customers) { item in
                     VStack(alignment: .center, spacing: 12) {
                         Image("\(item.logo)")
+                            .resizable()
                             .font(.largeTitle)
+                        
                         Text(item.name)
+                            .bold()
+                            .foregroundColor(.white)
                     }
+                    .frame(width: 100, height: 100)
                     .background(Color.secondary.opacity(0.67))
                     .cornerRadius(10)
                 }
             }
+            .padding()
             .navigationTitle("Our Customers")
         }
     }
