75504beec1f327c21f2c6400d5dfd4a1eb5704ae
Fito Toledano <fito.toledano@gmail.com>
2021-03-14 17:22:41 +0100

    wip

--- Nodeum/Views/Customers/Customers.swift
+++ Nodeum/Views/Customers/Customers.swift
@@ -11,29 +11,16 @@ struct Customers: View {
     
     private let customers = NodeumData.customers
     
-    let columns = [
-        GridItem(.adaptive(minimum: 80, maximum: 84),
-                 spacing: 12,
-                 alignment: .leading)
-    ]
-    
     var body: some View {
         NavigationView {
-            LazyVGrid(columns: columns, alignment: .center) {
-                ForEach(customers) { item in
-                    VStack(alignment: .center, spacing: 12) {
-                        Image("\(item.logo)")
-                            .resizable()
-                            .font(.largeTitle)
-                        
-                        Text(item.name)
-                            .bold()
-                            .foregroundColor(.white)
-                    }
-                    .frame(width: 100, height: 100)
-                    .background(Color.secondary.opacity(0.67))
+            List(customers) { customer in
+                Image(customer.logo)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 180)
+                    .padding()
+                    .background(Color.blue)
                     .cornerRadius(10)
-                }
             }
             .padding()
             .navigationTitle("Our Customers")
